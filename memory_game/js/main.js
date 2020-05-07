9922391d14a41c885ea8bda4230fb9e362646527
Kaisha Jones <kaishajones@Kaishas-Air.home>
2020-05-07 00:09:35 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("up and running!");
+
+let cardOne = "Queen";
+let cardTwo = "King";
+let cardThree = "Knight of Hearts";
+let cardFour = "Joker";
+
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardOne);
